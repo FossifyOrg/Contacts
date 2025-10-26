60562c0993596859e54f5837ca4e951410ff63a6
Naveen Singh <36371707+naveensingh@users.noreply.github.com>
2025-10-26 14:42:55 +0530

    feat: preserve search query on tab change

--- app/src/main/kotlin/org/fossify/contacts/activities/MainActivity.kt
+++ app/src/main/kotlin/org/fossify/contacts/activities/MainActivity.kt
@@ -417,7 +417,7 @@ class MainActivity : SimpleActivity(), RefreshContactsListener {
                 updateBottomTabItemColors(it.customView, false, getDeselectedTabDrawableIds()[it.position])
             },
             tabSelectedAction = {
-                binding.mainMenu.closeSearch()
+                getCurrentFragment()?.onSearchQueryChanged(binding.mainMenu.getCurrentQuery())
                 binding.viewPager.currentItem = it.position
                 updateBottomTabItemColors(it.customView, true, getSelectedTabDrawableIds()[it.position])
             }
